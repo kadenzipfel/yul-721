9d791d94ddf339e4eb87abf2de57b9c55ee7a067
kadenzipfel <kaden.zipfel@hotmail.com>
2022-06-12 20:37:55 -0700

    Use proper mock

--- src/test/ERC721.t.sol
+++ src/test/ERC721.t.sol
@@ -4,9 +4,33 @@ pragma solidity 0.8.10;
 import {DSTestPlus} from "solmate/test/utils/DSTestPlus.sol";
 import {DSInvariantTest} from "solmate/test/utils/DSInvariantTest.sol";
 
-import {MockERC721} from "solmate/test/utils/mocks/MockERC721.sol";
+import {ERC721TokenReceiver, ERC721} from "../ERC721.sol";
 
-import {ERC721TokenReceiver} from "../ERC721.sol";
+contract MockERC721 is ERC721 {
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+
+    function tokenURI(uint256) public pure virtual override returns (string memory) {}
+
+    function mint(address to, uint256 tokenId) public virtual {
+        _mint(to, tokenId);
+    }
+
+    function burn(uint256 tokenId) public virtual {
+        _burn(tokenId);
+    }
+
+    function safeMint(address to, uint256 tokenId) public virtual {
+        _safeMint(to, tokenId);
+    }
+
+    function safeMint(
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) public virtual {
+        _safeMint(to, tokenId, data);
+    }
+}
 
 contract ERC721Recipient is ERC721TokenReceiver {
     address public operator;
